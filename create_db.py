91a0074e2276bd71255dc79db00b5c6737ca7663
stieterd <peviho@outlook.com>
2024-02-05 00:17:17 +0100

    changes for the webapp

--- create_db.py
+++ create_db.py
@@ -4,7 +4,12 @@ mydb = mysql.connector.connect(
     host='localhost',
     user='root',
     passwd="Walvis12"
-)
+    )
 
 my_cursor = mydb.cursor()
-my_cursor.execute("CREATE DATABASE database")
+
+my_cursor.execute("CREATE DATABASE flights")
+
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+        print(db)
--- website/__init__.py
+++ website/__init__.py
@@ -4,7 +4,7 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database"
+DB_NAME = "flights"
 
 def create_app():
     app = Flask(__name__)
